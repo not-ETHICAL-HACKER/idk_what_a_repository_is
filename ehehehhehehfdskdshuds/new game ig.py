ebb2eaca0479e8080589e6c74995b89e4fe2450b
i-am-evil-hacker <johanwarrior333@gmail.com>
2025-09-13 17:38:55 +0530

    new update ig

--- ehehehhehehfdskdshuds/new game ig.py
+++ ehehehhehehfdskdshuds/new game ig.py
@@ -1,4 +1,37 @@
 #new password guessr game ig
 from random import randint
-r = randint(1000,9999)
-print("you are playing a gme abt how to find the password")
+from time import sleep
+n=1
+for i in range (2):
+    r = randint(1000,9999)
+    print("you are playing a gme abt how to find the password")
+    print(r)
+    sleep(1)
+    if r%2 != 0:
+        print("the last number is a odd number.")
+    else:
+        print("the last number is an even number.")
+    sleep(1)
+    r=r//10
+    if r%2 != 0:
+        print("the third number is a odd number.")
+    else:
+        print("the third number is an even number.")
+    r=r//10
+    sleep(1)
+    if r%2 != 0:
+        print("the second number is a odd number.")
+    else:
+        print("the second number is an even number.")
+    r=r//10
+    sleep(1)
+    if r%2 != 0:
+        print("the first number is a odd number.")
+    else:
+        print("the first number is an even number.")
+    print("would you like to break the password now?")
+    a=input("(y/n)")
+    if a=="y":
+        print("the password has to be cracked in 5 attempts or else you fucking die")
+        for i in range(5):
+            print(f"this is your {n} time cracking the code")
