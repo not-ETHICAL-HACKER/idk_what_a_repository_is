f0a7b04346a84f41218b4f5495b5ebbe522b82bd
not-ETHICAL-HACKER <johanwarrior0@gmail.com>
2025-09-19 19:35:22 +0530

    dont

---
+++ ehehehhehehfdskdshuds/dance.py
@@ -0,0 +1,17 @@
+import time
+n=0 
+a="|"
+b="\\"
+c="-"
+d="/"
+t=0.075
+while True:
+    n+=1
+    print(a*40)
+    time.sleep(t)
+    print(b*40)
+    time.sleep(t)
+    print(c*40)
+    time.sleep(t)
+    print(d*40)
+    time.sleep(t)
