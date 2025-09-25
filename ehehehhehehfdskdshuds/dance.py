2eae930af032a5f67270537821c652684ca9aef8
i-am-evil-hacker <johanwarrior333@gmail.com>
2025-09-25 11:43:20 +0530

    dsugfueiw

--- ehehehhehehfdskdshuds/dance.py
+++ ehehehhehehfdskdshuds/dance.py
@@ -1,2 +1,20 @@
-import pyautogui
-print(pyautogui.size())
+def fac(x:int):
+    if x==1:
+        return 1
+    else:
+        return x*fac(x-1)
+def gold_r(x:int|None = 1,n:int|None = 1):
+    nn=0
+    sign=1
+    l=[]
+    for i in range(1,n+1):
+        a=(x**nn)/fac(n)*sign
+        l.append(a)
+        sign*=-1
+        nn+=1
+        print(sum(l))
+    print(l)
+a=int(input("enter1:"))
+b=int(input("enter2:"))
+gold_r(a,b)
+
