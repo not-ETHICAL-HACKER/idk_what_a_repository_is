0c9b505b0eea1e5ee52e6c44cb99d72768ab4dc8
not-ETHICAL-HACKER <johanwarrior0@gmail.com>
2025-10-01 21:41:24 +0530

    Add solutions for LeetCode 2094: Find 3-digit even numbers
    Added two solution classes for LeetCode problem 2094, which finds all unique 3-digit even numbers that can be formed from a list of digits. Includes both an initial attempt (Solution2) and a more concise implementation (Solution).

--- leet code ans/1-10-2025.py
+++ leet code ans/1-10-2025.py
@@ -50,3 +50,68 @@ class Solution1:
 
         # Consume the remaining numBottles (less than numExchange).
         return consumed_bottles + numBottles
+#my solution for 2094 finding 3 digit even numbers from a array of digits
+class Solution2:
+    def __init__(self):
+        self.x=1
+    def findEvenNumbers(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        l=[]
+        res=[1]
+        results={}
+        len_d=len(digits)
+        for i in range(len_d):
+            for j in range(len_d):
+                for k in range(len_d):
+                    summ=0
+                    if digits[i]!=digits[j]!=digits[k] and digits[i]!=digits[k] and digits[k]%2==0 and digits[i]!=0:
+                        print(digits[i],digits[j],digits[k])
+                        hun_p=digits[i]*100
+                        ten_p=digits[j]*10
+                        one_p=digits[k]*1
+                        summ=hun_p+ten_p+one_p
+                        l.append(summ)
+                        scn=1
+                    else:
+                        continue
+        l.sort()
+        if digits[0]==digits[1] or digits[2]==digits[3]:
+            results=set(results)
+            len_d=len(digits)
+            for i in range(len_d):
+                for j in range(len_d):
+                    for k in range(len_d):
+                        summ=0
+                        print(digits[i],digits[j],digits[k])
+                        hun_p=digits[i]*100
+                        ten_p=digits[j]*10
+                        one_p=digits[k]*1
+                        summ=hun_p+ten_p+one_p
+                        results.add(summ)
+                        res=sorted(results)
+                        scn=2
+        if scn==1:
+            return l
+        if scn==2:
+            return res
+#actual solution
+class Solution:
+    def findEvenNumbers(self, digits:list):
+        nums = set()  # Target even set
+        n = len(digits)
+        # Traverse the indices of three digits
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    # Determine whether it meets the condition of the target even number
+                    if i == j or j == k or i == k:
+                        continue
+                    num = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    if num >= 100 and num % 2 == 0:
+                        nums.add(num)
+        # Converted to an array sorted in ascending order
+        res = sorted(list(nums))
+        return res
