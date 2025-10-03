7432a28327fc6193a6c153754f652dd58ecba845
not-ETHICAL-HACKER <johanwarrior0@gmail.com>
2025-10-03 08:28:34 +0530

    Add Two Sum solution to leetcode marathon script
    Appended a solution for the 'Two Sum' problem at the end of the leetcode 1 week marathon script. No functional changes were made to the golden ratio script.

--- leet code ans/leetcode 1 week marathon.py
+++ leet code ans/leetcode 1 week marathon.py
@@ -161,3 +161,15 @@ class Solution(object):
                 return "isosceles"
             elif num[0]!=num[1]!=num[2]:
                 return "scalene"
+#my solution for 1. Two Sum
+class Solution:
+    def twoSum(self, nums, target):
+        ans=[]
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i==j:
+                    continue
+                if nums[i]+nums[j]==target:
+                    ans.append(i)
+                    ans.append(j)
+                    return ans
