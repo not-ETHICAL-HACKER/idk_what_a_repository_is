a6c2a86f03838a92f794a07b06bc77007707b094
not-ETHICAL-HACKER <johanwarrior0@gmail.com>
2025-10-07 17:46:04 +0530

    Remove bot files and update golden ratio scripts
    Deleted bot1.bf and bot2.py from the bot directory. Refactored def.py to add new functions for level up, end screen, and fatigue handling, removing previous code. Added a new golden ratio using summation script and updated golden ratio.py to simplify the approximation logic by using b/a instead of summing lists.

--- bot/bot2.py
+++
@@ -1,8 +0,0 @@
-import time as t
-import random as r
-import pyautogui as p
-print(p.size())
-for a in range(10,768,50):
-    for b in range(10,1366,50):
-        p.moveTo(b,a)
-p.moveTo(420,69)
--- ehehehhehehfdskdshuds/def.py
+++ ehehehhehehfdskdshuds/def.py
@@ -1,102 +1,38 @@
-from time import sleep, time
-def plane():
-    a=0
-    b=0
-    while True:
-        if a>=100:
-            b=1
-            if b==1:
-                print("\n"*10)
-                print("""  _______   _        _     
- |__   __| | |      (_)    
-    | | ___| |_ _ __ _ ___ 
-    | |/ _ \ __| '__| / __|
-    | |  __/ |_| |  | \__ \\
-    |_|\___|\__|_|  |_|___/
-                           
-                           """)
-                break
-        print(" "*137+"|"*25)
-        print(" "*137+"|"*25)
-        print(" "*137+"|"*25)
-        print(" "*(17+a)+("_"*4)+(" "*3)+("_"*4)+(" "*8+"/")+(" "*1+"|"*25))
-        print(" "*(17+a)+("\\"*4)+(" "*3)+("/"*4)+(" "*7+"/")+(" "*2+"|"*25))
-        print(" "*(20+a)+("="*10)+"D>-<|"+(" "*3+"|"*25))
-        print(" "*(17+a)+("/"*4)+(" "*3)+("\\"*4)+(" "*7+"\\")+(" "*2+"|"*25))
-        print(" "*(17+a)+("¯"*4)+(" "*3)+("¯"*4)+(" "*8+"\\")+(" "*1+"|"*25))
-        print(" "*137+"|"*25)
-        print(" "*137+"|"*25)
-        print(" "*137+"|"*25)
-        a+=1
-        sleep(0.1)
-'''
-class solution:
-        def __init__(self,nums):
-            self.nums=nums
-        def sum_wo_ai(nums,target):
-            l=[]
-            for i in range(len(nums)):
-                for j in range(len(nums)):
-                    if nums[i]+nums[j]==target:
-                        l.append([i])
-                        print(l)
-            return l
-solution.sum_wo_ai([1,23,67,73,2,1],69)'''
-l=[
-    [
-        [1,2,3],
-        [2,3,1],
-        [3,2,1]
-    ],
-    [
-        [4,5,6],
-        [5,6,4],
-        [6,5,4]
-    ],
-    [
-        [7,8,9],
-        [8,9,7],
-        [9,8,7]
-    ]
-]
-import random
-import colorama
-import pyautogui
+def lvl_up(exp):
+    lvl=lvl+(exp//5)
+    exp=exp%5
+    luck=luck
+    hp=hp+(10*lvl)
+    dmg=dmg+(10*lvl)
+    mp=mp+(10*lvl)
+    dex=dex+(10*lvl)
+    ("your currrent level is:",lvl)
+    print("Your stats are:\nSTR:",dmg,"\nMP:",mp,"\nHP:",hp,"\nDEX:",dex,"\nLUCK:",luck)
+    return exp,lvl
 import time
-print(pyautogui.size())
-def bet_print(printf,jumbler=False,font=False,sussy_mode=False,seed: int=1):
-        #seed id how the randomiser and all other stuff work ie a cheat code
-        if seed is not None:
-            random.seed(seed)
-        if jumbler==False and font==False and sussy_mode==False:
-            print(printf)
-        if jumbler==True and sussy_mode==True and font==True:
-            print("no,im not doing that")
-
-        if jumbler==True:
-            for _ in range(len(printf)):
-                ra=random.randrange(0,len(printf))
-                print(printf[ra],end="")
-            print()
-        if font==True:
-            for ch in printf:
-                if ch.isalpha():
-                    print(ch.upper(),end="")
-                else:
-                    print(ch,end="")
-        if sussy_mode==True:
-            print("\nඞ Sussy baka detected! ඞ")
-            print('ඞ'*10)
-            if sussy_mode==True and font==True:
-                for ch in printf:
-                    if ch.isalpha():
-                        print(ch.upper(),end="")
-                    else:
-                        print(ch,end="")
-                    print("ඞ",end="")
-from colorama import init, Fore, Style
-init(autoreset=True)
-print(Fore.RED + "This should be red!")
-print(Style.BRIGHT + "This should be bright!")
-print(Fore.GREEN + Style.DIM +"This should be dim green!")
-time.sleep(20)
+import sys
+def d_end_scr():
+    print("\t\U00002620YOU HAVE DIED\U00002620")
+    time.sleep(0.5)
+    print("\t\t\tEND SCREEN")
+    print("Your stats are:\nSTR:",dmg,"\nMP:",mp,"\nHP:",hp,"\nDEX:",dex,"\nLUCK:",luck)
+    print(f"Thank you for playing my game;)")
+    sys.exit()
+def fatigue(fatigue):
+    if fatigue>100:
+        print("you are too fatigued")
+        dmg=dmg/2
+        hp=hp/2
+        mp=mp/2
+        dex=dex/2
+        print("Your stats are:\nSTR:",dmg,"\nMP:",mp,"\nHP:",hp,"\nDEX:",dex,"\nLUCK:",luck)
+    elif fatigue>200:
+        print("you are now super fatiged")
+        dmg=dmg/4
+        hp=hp/4
+        mp=mp/4
+        dex=dex/4
+        print(f"")
+    else:
+        print("you are fatigued but can still function")
+        fatigue+=10
